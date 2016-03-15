c5a5ba15af594a5af3dec25ad2917bbbf2bc1fd5
Victor Konen <vkonen1@umbc.edu>
2016-03-15 00:24:14 -0400

    PHP script to convert mySQL DB info to JSON; PHP script to display JSON using JavaScript; JavaScript to display JSON;

---
+++ json_tester/json_tester.html.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>JSON Tester</title>
+		<link rel="stylesheet" href="json_tester.css" />
+		<!-- get the encoded json from php for javascript -->
+		<script type="text/javascript">var courses_json = <?php echo $courses_json; ?>;</script>
+		<script type="text/javascript" src="json_tester.js"></script>
+	</head>
+	<body>
+		<div id="main"></div>
+	</body>
+</html>
---
+++ json_tester/json_update.php
@@ -0,0 +1,101 @@
+<?php
+//name of the json file
+$json_file = "../json/courses.json";
+
+//mysql server connection info
+$server = "localhost";
+$username = "cmsc433";
+$password = "CMSC433group";
+
+//connect to the mysql server
+$mysql = mysql_connect($server, $username, $password);
+if (!$mysql) {
+	die("Could not connect to the database: " . mysql_error());
+}
+
+//select the cmsc433 database
+$db = mysql_select_db("cmsc433", $mysql);
+
+//query for the data
+$query = "SELECT c.*, p.* FROM Courses c INNER JOIN Courses_Prereqs cp ON cp.course_id = c.course_id INNER JOIN Prereqs p ON p.prereq_id = cp.prereq_id";
+$result = mysql_query($query);
+if (!$result) {
+	die("Error: " . mysql_error() . "<br />Query: " . $query);
+}
+
+/*while ($course = mysql_fetch_assoc($result)) {
+	var_dump($course);
+	echo "<br /><br />";
+}
+exit;*/
+
+//start of the json file
+$json = "{\n";
+$json .= "\t\"courses\": [\n";
+//process the data
+$course = mysql_fetch_assoc($result);
+while ($course) {
+	//want pretty formatting so the json is readable
+	//add the info for the course
+	$json .= "\t\t{\n";
+	$json .= "\t\t\t\"id\": \"" . $course["course_id"] . "\",\n";
+	$json .= "\t\t\t\"type\": \"" . $course["course_type"] . "\",\n";
+	$json .= "\t\t\t\"credits\": \"" . $course["course_credits"] . "\",\n";
+	$json .= "\t\t\t\"required\": \"" . $course["course_required"] . "\",\n";
+	$json .= "\t\t\t\"name\": \"" . $course["course_name"] . "\",\n";
+	$json .= "\t\t\t\"desc\": \"" . $course["course_desc"] . "\",\n";
+
+	//need to add the array of prerequisite courses
+	$json .= "\t\t\t\"prereqs\": [";
+	$prereqs = intval($course["course_prereqs"]);
+	//none to add
+	if ($prereqs == 0) {
+		$json .= "]\n";
+	} else {
+		$passed_self = false;
+		//iterate over all associated rows
+		for ($i = 0; $i < $prereqs; $i++) {
+			//dont need to note self as prerequisite
+			if ($course["course_id"] == $course["prereq_id"]) {
+				$passed_self = true;
+				$course = mysql_fetch_assoc($result);
+			}
+
+			//generate the prerequisite course for json
+			$json .= "\n\t\t\t\t{\n";
+			$json .= "\t\t\t\t\t\"id\": \"" . $course["prereq_id"] . "\",\n";
+			$json .= "\t\t\t\t\t\"type\": \"" . $course["prereq_type"] . "\"\n";
+			$json .= "\t\t\t\t}";
+
+			if ($prereqs > 1 && $i < $prereqs - 1) {
+				$json .= ",";
+			}
+
+			if ($i < $prereqs - 1 || !$passed_self) {
+				//get the next associated row
+				$course = mysql_fetch_assoc($result);
+			}
+		}
+		$json .= "\n\t\t\t]\n";		
+	}
+	//next row for the next iteration
+	$course = mysql_fetch_assoc($result);
+
+	//end of course syntax
+	$json .= "\t\t}";
+	if ($course) {
+		$json .= ",";
+	}
+	$json .= "\n";
+}
+$json .= "\t]\n";
+$json .= "}";
+
+//write to the file
+file_put_contents($json_file, $json);
+
+//output for testing
+//echo "<pre>" . $json . "</pre>";
+
+//close the mysql server connection
+mysql_close($mysql);
