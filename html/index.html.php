91afedd49107973e0c5243b1d6df03c85bc4c71f
Victor <vkonen1@umbc.edu>
2016-03-22 17:17:16 -0400

    Cleaned up html tags to get ready for javascript; Fixed css to match;

--- html/index.html.php
+++ html/index.html.php
@@ -1,60 +1,56 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>CMSC Degree Tracker</title>
-	<link rel="stylesheet" href="css/index.css">
-	<!-- get the encoded json from php for javascript -->
-	<script type="text/javascript">var courses_json = <?php echo($courses_json); ?>;</script>
-	<script type="text/javascript" src="js/courses.js"></script>
-</head>
+	<head>
+		<title>CMSC Degree Tracker</title>
+		<link rel="stylesheet" href="css/index.css">
+		<!-- get the encoded json from php for javascript -->
+		<script type="text/javascript">var courses_json = <?php echo($courses_json); ?>;</script>
+		<script type="text/javascript" src="js/courses.js"></script>
+	</head>
 
-<body>
-<h1>CMSC Degree Tracker</h1>
+	<body>
+		<h1>CMSC Degree Tracker</h1>
 
-<div id="northWest">
-<form>
-	ID #:
-	<input type="text" name="idnum">
-	<input type="submit" value="Submit">
-	<br> <br> <br>
-</form>
-<form>
-	<input type="submit" class="optionButton" value="CMSC">
-	<input type="submit" class="optionButton" value="SCI">
-</form>
-</div>
+		<div id="northWest">
+			<form>
+				ID #:
+				<input type="text" name="idnum">
+				<input type="submit" value="Submit">
+				<br> <br> <br>
+			</form>
+			<form>
+				<input type="submit" class="optionButton" value="CMSC">
+				<input type="submit" class="optionButton" value="SCI">
+			</form>
+		</div>
 
-<div id="northEast">
-<textarea disabled>
-Tooltip?
-</textarea>
-</div>
+		<div id="northEast">
+			<textarea disabled>
+			Tooltip?
+			</textarea>
+		</div>
 
-<p id="clearLeft"></p>
+		<div class="clear"></div>
 
-<!--  Select Box -->
-<div id="south">
-<select size="18">
+		<!--  Select Box -->
+		<div class="course-section">
+			<select id="course-available" size="18"></select>
+			<p>Class Available</p>
+		</div>
 
-</select>
-<p>Class Available</p>
-</div>
+		<div class="course-section">
+			<select id="course-taken" size="18"></select>
+			<p>Classes Taken</p>
+		</div>
 
-<div id="south">
-<select size="18">
+		<div class="course-section">
+			<select id="course-options" size="18"></select>
+			<p>Class Options</p>
+		</div>
+		<!-- End of Select Box-->
 
-</select>
-<p>Classes Taken</p>
-</div>
-
-<div id="south">
-<select size="18">
-	
-</select>
-<p>Class Options</p>
-</div>
-<!-- End of Select Box-->
-<div style="clear: both;"></div>
-<div id="main"></div>
-</body>
+		<!-- For debugging class population -->
+		<div class="clear"></div>
+		<div id="main"></div>
+	</body>
 </html>
